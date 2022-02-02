6e0a27bd3bff902dd2ac80a3cba4ed518091a41b
Abtinz <abtinzandi@gmail.com>
2022-02-02 19:19:53 +0330

    module_7

---
+++ module_7/controller.v
@@ -0,0 +1,39 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2021-2022
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: controller
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module controller(
+input [2:0] system_token,
+input request,
+input [7:0] TimeData,
+input reset,
+input clock,
+input confirm,
+input [2:0] user_token,
+output [7:0] data_Q,
+output [7:0] data_P
+);
+
+reg [7:0] data_Q;
+reg [7:0] data_P;
+
+register Pregister(TimeData,clock,request,confirm);
+register Qregister(TimeData,clock,request,confirm);
+
+endmodule
